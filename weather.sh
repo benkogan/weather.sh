862332bd392642ca9557012fa4de292b6f26afc0
Ben Kogan <benkogan@me.com>
2014-05-25 17:26:20 -0400

    Moved most ops into functions, still messy/incomplete

--- weather.sh
+++ weather.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 
 # By Ben Kogan (http://benkogan.com)
-# Usage:
-#       weather [-ctv]
-#
-# [-t]  for tmux -- uses cache file
-# [-c]  check in Celcius; default is Fahrenheit
+
+VERSION="0.0.1"
 
 cachedir="$HOME/scripts/.cache"
 cache="$HOME/scripts/.cache/weather.log"
@@ -17,71 +14,118 @@ modnew=5   # 5 seconds
 # TODO: change ping interval to 1 min or respond to wifi signal?
 # TODO: respond to curl failure
 
-
 ## output usage
 usage () {
-    echo "usage: weather [-ctv]"
+    echo "usage: weather [-chtv]"
 }
 
-# tmux
-if [[ "$1" == "-t" ]]; then
+## get zipcode
+zipcode () {
+    zipcode=$(curl --silent "http://ipinfo.io/" | grep -e '(postal|})' \
+        | sed -e 's/"postal": "//' -e 's/}//' -e 's/"//' | tr -d '\n  ')
+}
 
-    # If .cache dir doesn't exist, make it
-    if [[ ! -d "$cachedir" ]]; then
-        mkdir "$cachedir"
-        touch "$cache"
-        error=$?
-        if [ ! "$error" -eq 0 ]; then
-            echo "mkdir $cachedir failed"
-            exit $error
+# get weather
+getweather () {
+    if [[ "$2" == "-c" || "$1" == "-c" ]]; then
+        weather=$(curl --silent \
+            "http://xml.weather.yahoo.com/forecastrss?p=$zipcode&u=c" \
+            | grep -E '(Current Conditions:|C<BR)' \
+            | sed -e 's/Current Conditions://' -e 's/<br \/>//' -e 's/<b>//' \
+            -e 's/<\/b>//' -e 's/C<BR \/>/ºC/' -e 's/<description>//' \
+            -e 's/<\/description>//' | tr -d '\n')
+    else
+        weather=$(curl --silent \
+            "http://xml.weather.yahoo.com/forecastrss?p=$zipcode" \
+            | grep -E '(Current Conditions:|F<BR)' \
+            | sed -e 's/Current Conditions://' -e 's/<br \/>//' -e 's/<b>//' \
+            -e 's/<\/b>//' -e 's/F<BR \/>/°F/' -e 's/<description>//' \
+            -e 's/<\/description>//' | tr -d '\n')
+    fi
+    echo "$weather"
+}
+
+## tmux
+tmux () {
+    if [[ "$1" == "-t" ]]; then
+
+        # If .cache dir doesn't exist, make it
+        if [[ ! -d "$cachedir" ]]; then
+            mkdir "$cachedir"
+            touch "$cache"
+            error=$?
+            if [ ! "$error" -eq 0 ]; then
+                echo "mkdir $cachedir failed"
+                exit $error
+            fi
         fi
+
+        cachemod=$(stat -f%m "$cache") # Mod time of cache in sec since the epoch
+        let diff=($time - $cachemod)   # Seconds from now since file was modified
+
+        echo "$diff"
+
+        # First case:  on tmux startup; assumes this is the case if cache file
+        #              hasn't been used for longer than modold
+        # Second case: cache is newly created; should thus be empty
+        if [[ $diff -gt $modold || $diff -lt $modnew ]]; then
+            # TODO: maybe this is a bad idea? delays loading twice as long
+            echo "loading"
+
+        # Load cached version
+        elif [[ $diff -lt $modmid ]]; then
+            echo "cache test"
+            read weather < $cache
+            echo "$weather"
+            exit $?
+        fi # If diff is between modmid and modold, continues
     fi
 
-    cachemod=$(stat -f%m "$cache") # Mod time of cache in sec since the epoch
-    let diff=($time - $cachemod)   # Seconds from now since file was modified
+    zipcode
+    getweather
 
-    echo "$diff"
+    # tmux: add weather to cache file
+    if [ "$1" == "-t" ]; then
+        echo "$weather" > "$cache"
+    fi
+}
 
-    # First case:  on tmux startup; assumes this is the case if cache file
-    #              hasn't been used for longer than modold
-    # Second case: cache is newly created; should thus be empty
-    if [[ $diff -gt $modold || $diff -lt $modnew ]]; then
-        # TODO: maybe this is a bad idea? delays loading twice as long
-        echo "loading"
+## main
+weather () {
+    local arg="$1"
 
-    # Load cached version
-    elif [[ $diff -lt $modmid ]]; then
-        echo "cache test"
-        read weather < $cache
-        echo "$weather"
-        exit $?
-    fi # If diff is between modmid and modold, continues
-fi
+    case "${arg}" in
 
-# Get zipcode
-zipcode=$(curl --silent "http://ipinfo.io/" | grep -E '(postal|})' \
-    | sed -e 's/"postal": "//' -e 's/}//' -e 's/"//' | tr -d '\n  ')
+        ## flags
+        -V|--version)
+            echo "${VERSION}"
+            return 0
+            ;;
 
-# Get weather
-if [[ "$2" == "-c" || "$1" == "-c" ]]; then
-    weather=$(curl --silent \
-        "http://xml.weather.yahoo.com/forecastrss?p=$zipcode&u=c" \
-        | grep -E '(Current Conditions:|C<BR)' \
-        | sed -e 's/Current Conditions://' -e 's/<br \/>//' -e 's/<b>//' \
-        -e 's/<\/b>//' -e 's/C<BR \/>/ºC/' -e 's/<description>//' \
-        -e 's/<\/description>//' | tr -d '\n')
+        -h|--help)
+            usage
+            return 0
+            ;;
+
+        -c|--celcius)
+            celcius
+            return 0
+            ;;
+
+        -t|--tmux)
+            tmux
+            return 0
+            ;;
+
+    esac
+    getweather
+}
+
+## export or run
+if [[ ${BASH_SOURCE[0]} != $0 ]]; then
+    export -f weather
 else
-    weather=$(curl --silent \
-        "http://xml.weather.yahoo.com/forecastrss?p=$zipcode" \
-        | grep -E '(Current Conditions:|F<BR)' \
-        | sed -e 's/Current Conditions://' -e 's/<br \/>//' -e 's/<b>//' \
-        -e 's/<\/b>//' -e 's/F<BR \/>/°F/' -e 's/<description>//' \
-        -e 's/<\/description>//' | tr -d '\n')
-fi
-echo "$weather"
-
-# tmux: add weather to cache file
-if [ "$1" == "-t" ]; then
-    echo "$weather" > "$cache"
+    weather "{@}"
+    exit $?
 fi
 
